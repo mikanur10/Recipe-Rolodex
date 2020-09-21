9ac28e5088cef1a95ac9c576b646c6e80bc5e80f
Mika Nur <mikanur@Mikas-MBP.fios-router.home>
2020-09-21 13:44:43 -0400

    js starter

---
+++ app.js
@@ -0,0 +1,10 @@
+Access - Control - Allow - Origin: *
+  
+const input = document.querySelector('#blank');
+const button = document.querySelector('#search');
+
+button.addEventListener('click', async () => {
+  let userInput = input.value;
+  const response = await axios.get(`http://www.recipepuppy.com/api/?q=${userInput}`)
+  renderList(response.data.results)
+});
